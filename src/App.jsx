47af123ea5d92b36d4a6b6003224f68db77559b4
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-15 20:07:36 +0700

    Passed props to List and render it

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 
   return (
     <main>
-      <List />
+      <List people={people} />
     </main>
   );
 };
--- src/List.jsx
+++ src/List.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Person from "./Person";
 
-const List = () => {
-  return <div>List</div>;
+const List = ({ people }) => {
+  return (
+    <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      {people.map((person) => {
+        return <Person key={person.id} {...person} />;
+      })}
+    </section>
+  );
 };
 
 export default List;
